e1d776855d953981633a04f9f33b1a5e7186c058
Danielsoliveira44 <ds5676419@gmail.com>
2023-03-22 13:45:44 -0300

    Adicionar arquivos de projeto.

---
+++ TintSysDesk/TintSys/ClassLibrary2/Nivel.cs
@@ -0,0 +1,22 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TintSysClass
+{
+    public class Nivel
+    {
+        public Nivel()
+        {
+            var cmd = Banco.Abrir();
+        }
+
+        public void Inserir();
+        {
+
+        }
+    }
+}
